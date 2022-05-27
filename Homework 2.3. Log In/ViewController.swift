50f5e168f6f86fe65ca8a6669890ef1314a65ed2
Angelina <gellaproios@gmail.com>
2022-05-27 17:44:24 +0700

    Initial Commit

---
+++ Homework 2.3. Log In/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 2.3. Log In
+//
+//  Created by Павел Прохоренко on 27.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
